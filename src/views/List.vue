39b5c6afa22e9555bff5767ff04f30710edfe8e1
fishu <pkaczoruk@wp.pl>
2020-02-27 00:21:52 +0100

    install axios, get data from swapi

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="list">
-
-  </div>
+  <v-container>
+    
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'List'
+  name: 'List',
+  data() {
+    return {
+      people: [],
+      planets: []
+    }
+  },
+  created() {
+    // get data from swapi
+    axios.get('https://swapi.co/api/people/')
+      .then(res => this.people = res.data)
+      .catch(() => {alert('There was unexpected error. Please try to reload page')});
+    axios.get('https://swapi.co/api/planets/')
+      .then(res => this.planets = res.data)
+      .catch(() => {alert('There was unexpected error. Please try to reload page')});
+  }
 }
 </script>
 
