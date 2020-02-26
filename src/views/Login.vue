f2d66c6c4de6e11a4f39cf69acd7cc7fe5c72f43
fishu <pkaczoruk@wp.pl>
2020-02-26 23:47:27 +0100

    create form, basic validation and add styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,62 @@
 <template>
-  <div class="login">
+  <v-container fill-height>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-form 
+        class="text-center ml-4"
+        ref="login"
+      >
+        <h2 class="mb-8 text-center primary--text">Sign in to browse content</h2>
 
-  </div>
+        <v-text-field
+          type=""
+          v-model="auth.login"
+          label="Login"
+          required
+          :rules="rules.login"
+          append-icon="mdi-account"
+          outlined></v-text-field>
+
+        <v-text-field
+          type=""
+          v-model="auth.password"
+          label="Password"
+          required
+          :rules="rules.password"
+          append-icon="mdi-lock"
+          outlined></v-text-field>
+
+        <v-btn class="primary" @click="onLogin()">
+          Send
+        </v-btn>          
+      </v-form>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      auth: {
+        login: '',
+        password: ''
+      },
+      rules: {
+        login: [ v => v.length >= 1 || 'Login is required' ],
+        password: [ v => v.length >= 1 || 'Password is required' ]
+      }
+    }
+  },
+  methods: {
+    onLogin() {
+      // validate
+      this.$refs.login.validate()
+    }
+  }
 }
 </script>
 
