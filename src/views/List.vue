8aed556639d50a5675fe16ddb84e9a19ed5a1e6b
fishu <pkaczoruk@wp.pl>
2020-02-27 02:09:01 +0100

    add tables for both planets and people. Change way of getting data from api

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,48 @@
 <template>
   <v-container>
     
+    <v-card max-width="600px">
+      <v-card-title>
+        People
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="people.search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="people.headers"
+        :items="people.list"
+        :search="people.search"
+        items-per-page="5"
+        :loading="people.loading"
+      ></v-data-table>
+    </v-card>
+
+    <v-card max-width="600px">
+      <v-card-title>
+        Planets
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="planets.search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="planets.headers"
+        :items="planets.list"
+        :search="planets.search"
+        items-per-page=5
+        :loading="planets.loading"
+      ></v-data-table>
+    </v-card>
+
   </v-container>
 </template>
 
@@ -11,18 +53,51 @@ export default {
   name: 'List',
   data() {
     return {
-      people: [],
-      planets: []
+      people: {
+        search: '',
+        list: [],
+        headers: [
+          { text: 'Name', value: 'name' },
+          { text: 'Height', value: 'height', filterable: false },
+          { text: 'Gender', value: 'gender', filterable: false },
+          { text: 'Birth Year', value: 'birth_year', filterable: false },
+        ],
+        loading: true
+      },
+      planets: {
+        search: '',
+        list: [],
+        headers: [
+          { text: 'Name', value: 'name' },
+          { text: 'Diameter', value: 'diameter', filterable: false },
+          { text: 'Terrain', value: 'terrain', filterable: false },
+          { text: 'Population', value: 'population', filterable: false },
+        ],
+        loading: true
+      }
+    }
+  },
+  methods: {
+    // get data from api
+    getData(apiURL, resource) {
+      axios.get(apiURL)
+        .then(res => this.addData(res.data, resource))
+        .catch(err => alert(err));
+    },
+
+    // add data to array
+    addData(data, resource) {
+      for(let i=0; i<data.results.length; i++) {
+        this[resource].list.push(data.results[i]);
+      }
+
+      data.next ? this.getData(data.next, resource) : this[resource].loading = false;
     }
   },
   created() {
-    // get data from swapi
-    axios.get('https://swapi.co/api/people/')
-      .then(res => this.people = res.data)
-      .catch(() => {alert('There was unexpected error. Please try to reload page')});
-    axios.get('https://swapi.co/api/planets/')
-      .then(res => this.planets = res.data)
-      .catch(() => {alert('There was unexpected error. Please try to reload page')});
+    const apiURL = 'https://swapi.co/api/';
+    this.getData(apiURL+'people', 'people');
+    this.getData(apiURL+'planets', 'planets');
   }
 }
 </script>
